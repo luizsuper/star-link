5e27c3464320f0ef8d7c9111da1d34a03e3e8f64
luizsuper <luizsuper@163.com>
2021-07-20 21:41:03 +0800

    feat : 新增了fish命令

--- cmd/corn/server.go
+++ cmd/corn/server.go
@@ -1,8 +1,15 @@
+package corn
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+)
+
 var StartCmd = &cobra.Command{
 	Use:   "fish",
-	Short: "fish的第一次尝试",
-	Long: `fish的第一次尝试fish的第一次尝试fish的第一次尝试fish的第一次尝试`,
+	Short: "fish sss",
+	Long:  `fish`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("我是崔可鱼")
+		fmt.Println("fish sss")
 	},
 }
